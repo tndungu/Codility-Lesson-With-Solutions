a6f84716c102e5b7b110ca99da70b7b74bc3c9d3
tndungu <tmndungu@gmail.com>
2022-02-10 16:10:25 +0200

    Create Solution.js

---
+++ Lesson 1 - Iterations/BinaryGap/Solution.js
@@ -0,0 +1,18 @@
+function solution(N) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    let binaryGap = 0;
+    let start = false,
+        tempCount = -1;
+
+    while(N > 0){
+        if(N%2 == 1){
+            binaryGap = tempCount > binaryGap ? tempCount : binaryGap;
+            tempCount = 0;
+        }
+        else if(tempCount !== -1){
+                tempCount++;
+        }
+        N = Math.floor(N/2);
+    }
+    return binaryGap;
+}
